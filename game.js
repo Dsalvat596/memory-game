9c55ea6158a0406f9c97ef0e0294f01395402ec4
dsalvat596 <dsalvat596@gmail.com>
2018-09-23 14:56:13 -0400

    need to fix disable/enable functions

--- game.js
+++ game.js
@@ -1,16 +1,31 @@
 const cardIcons = ['gem', 'plane', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb' ];
+let openedCards = [];
 let cardLoop = 0;
 let list = $('.deck');
+let moves = 0;
+let counter = $('.moves')
 
-const createCards = () => {
+//declare cards globally
+let cards = [...$('.card')]  //... spread converts nodelist to true array
+
+// declaring variable of matchedCards
+let matchedCard = $(".match");
+const startGame = () => {
     while(cardLoop < 2){
         shuffleCards(cardIcons);
         for(let i = 0; i <cardIcons.length; i++){
-            list.append( `<li class="card match" type="${cardIcons[i]}">
+            list.append( `<li class="card" type="${cardIcons[i]}">
             <i class="fa fa-${cardIcons[i]}"></i>
         </li>`)
         }
         cardLoop +=1
+    }
+    //collect all cards that were created, and add event listener to each
+    let cards = [...$('.card')]  //... spread converts nodelist to true array
+    for(let j = 0; j < cards.length; j++){
+        card = cards[j]
+        card.addEventListener("click", displayCard)
+        card.addEventListener("click", cardOpen);
     }
 }
 
@@ -27,5 +42,73 @@ const shuffleCards = (array) => {
         array[j] = temp
     }
 }
+const displayCard = function (){
+    this.classList.toggle("open");
+    this.classList.toggle("show");
+    this.classList.toggle("disabled");
+    
+}
 
-createCards();
+// check for matches
+
+const cardOpen = function () {
+    openedCards.push(this)
+    let len = openedCards.length;
+    if(openedCards.length === 2){
+        moveCounter();
+        console.log(moves)
+        if(openedCards[0].type === openedCards[1].type){
+            matched();
+        } else {
+            unMatched();
+        }
+    }
+}
+
+
+//handle matches
+const matched = function(){
+    openedCards[0].classList.add("match");
+    openedCards[1].classList.add("match");
+    openedCards[0].classList.remove("show", "open");
+    openedCards[1].classList.remove("show", "open");
+    openedCards = [];
+}
+
+//handle unmatches
+
+const unMatched = function(){
+    openedCards[0].classList.add("unmatched");
+    openedCards[1].classList.add("unmatched");
+    disable();
+    setTimeout(function(){
+        openedCards[0].classList.remove("show", "open", "unmatched");
+        openedCards[1].classList.remove("show", "open", "unmatched");
+        enable();
+        openedCards = [];
+    },1100);  
+}
+
+//disable cards temporarily
+function disable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.add('disabled');
+    });
+}
+
+//enable cards and disable matched cards
+function enable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.remove('disabled');
+        for(let i = 0; i < matchedCard.length; i++){
+            matchedCard[i].classList.add("disabled");
+        }
+    });
+}
+
+function moveCounter(){    
+    moves++;    
+    counter.innerText = moves;
+}
+
+window.onload = startGame();
