018226ca7db8063e2bddd268e409650a11155ffe
dsalvat596 <dsalvat596@gmail.com>
2018-10-02 22:27:59 -0400

    game over modal, restart game, fixed bugs

--- game.js
+++ game.js
@@ -1,112 +1,119 @@
-const cardIcons = ['gem', 'plane', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb' ];
+const cardIcons = [
+  "gem",
+  "plane",
+  "anchor",
+  "bolt",
+  "cube",
+  "leaf",
+  "bicycle",
+  "bomb"
+];
 let openedCards = [];
 let cardLoop = 0;
-let list = $('.deck');
+let list = $(".deck");
 let moves = 0;
-let counter = $('.moves')
-let restartButton = $('.restart')[0]
+let counter = $(".moves");
+let restartButton = $(".restart")[0];
+let close = $(".close");
 
-restartButton.addEventListener("click", restart)
+restartButton.addEventListener("click", restart);
 
 //declare cards globally
-let cards = [...$('.card')]  //... spread converts nodelist to true array
+let cards = [...$(".card")]; //... spread converts nodelist to true array
 
 // declaring variable of matchedCards
 let matchedCard = [...$(".match")];
 
 document.body.onload = startGame();
 
-function startGame (){
-    moves = 0;
-    cardLoop = 0;
-    counter.html(moves);
-    list.html('');
-    while(cardLoop < 2){
-        shuffleCards(cardIcons);
-        for(let i = 0; i <cardIcons.length; i++){
-            list.append( `<li class="card" type="${cardIcons[i]}">
+function startGame() {
+  moves = 0;
+  cardLoop = 0;
+  counter.html(moves);
+  list.html("");
+  while (cardLoop < 2) {
+    shuffleCards(cardIcons);
+    for (let i = 0; i < cardIcons.length; i++) {
+      list.append(`<li class="card" type="${cardIcons[i]}">
             <i class="fa fa-${cardIcons[i]}"></i>
-        </li>`)
-        }
-        cardLoop +=1
+        </li>`);
     }
-    //collect all cards that were created, and add event listener to each
-    let cards = [...$('.card')]  //... spread converts nodelist to true array
-    for(let j = 0; j < cards.length; j++){
-        card = cards[j]
-        card.addEventListener("click", displayCard)
-        card.addEventListener("click", cardOpen);
-    }
+    cardLoop += 1;
+  }
+  //collect all cards that were created, and add event listener to each
+  let cards = [...$(".card")]; //... spread converts nodelist to true array
+  for (let j = 0; j < cards.length; j++) {
+    card = cards[j];
+    card.addEventListener("click", displayCard);
+    card.addEventListener("click", cardOpen);
+  }
 }
 
 // array randomizer algorithm
-function shuffleCards(array){
-    var i = 0
-        , j = 0
-        , temp = null
+function shuffleCards(array) {
+  var i = 0,
+    j = 0,
+    temp = null;
 
-    for (i = array.length - 1; i > 0; i -= 1) {
-        j = Math.floor(Math.random() * (i + 1))
-        temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
-    }
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
 function displayCard() {
-    this.classList.toggle("open");
-    this.classList.toggle("show");
-    this.classList.toggle("disabled");
-    
+  this.classList.toggle("open");
+  this.classList.toggle("show");
+  this.classList.toggle("disabled");
 }
 
 // check for matches
 
 function cardOpen() {
-    openedCards.push(this)
-    if(openedCards.length === 2){
-        moveCounter();
-        console.log(moves)
-        if(openedCards[0].type === openedCards[1].type){
-            matched();
-            if(matchedCard.length === cardIcons.length){
-                setTimeout(function(){
-                    alert(`you won nibba!  It took you ${moves} tries!`);
-                startGame();
-                }, 500);
-            }
-        } else {
-            unMatched();
-        }
+  openedCards.push(this);
+  if (openedCards.length === 2) {
+    moveCounter();
+    console.log(moves);
+    if (openedCards[0].type === openedCards[1].type) {
+      matched();
+      if (matchedCard.length === cardIcons.length) {
+        setTimeout(function() {
+          gameOver();
+        }, 500);
+      }
+    } else {
+      unMatched();
     }
-};
-
+  }
+}
 
 //handle matches
-const matched = function(){
-    openedCards[0].classList.add("match");
-    openedCards[1].classList.add("match");
-    openedCards[0].classList.remove("show", "open");
-    openedCards[1].classList.remove("show", "open");
-    openedCards = [];
-    matchedCard.push(this.card);
-    console.log(matchedCard);
-}
+const matched = function() {
+  openedCards[0].classList.add("match");
+  openedCards[1].classList.add("match");
+  openedCards[0].classList.remove("show", "open");
+  openedCards[1].classList.remove("show", "open");
+  openedCards = [];
+  matchedCard.push(this.card);
+  console.log(matchedCard);
+};
 
 //handle unmatches
 
-const unMatched = function(){
-    openedCards[0].classList.add("unmatched");
-    openedCards[1].classList.add("unmatched");
-    //disable();
-    setTimeout(function(){
-        openedCards[0].classList.remove("show", "open", "unmatched");
-        openedCards[1].classList.remove("show", "open", "unmatched");
-        openedCards[0].classList.remove("disabled");
-        openedCards[1].classList.remove("disabled");
-       // enable();
-        openedCards = [];
-    },1100);  
-}
+const unMatched = function() {
+  openedCards[0].classList.add("unmatched");
+  openedCards[1].classList.add("unmatched");
+  //disable();
+  setTimeout(function() {
+    openedCards[0].classList.remove("show", "open", "unmatched");
+    openedCards[1].classList.remove("show", "open", "unmatched");
+    openedCards[0].classList.remove("disabled");
+    openedCards[1].classList.remove("disabled");
+    // enable();
+    openedCards = [];
+  }, 1100);
+};
 
 //disable cards temporarily
 // function disable(){
@@ -117,24 +124,29 @@ const unMatched = function(){
 
 //enable cards and disable matched cards
 // function enable(){
-    
+
 //     console.log(matchedCard);
-    
+
 // }
 //updates the moves counter
-function moveCounter(){    
-    moves++;    
-    counter.html(moves);
-};
-
-
-//handles game over
-function gameOver(){
-    alert(`You Win!  It took you ${moves} moves!`)
+function moveCounter() {
+  moves++;
+  counter.html(moves);
 }
+
+//handles game over/popup modal
+function gameOver() {
+  $("#modalText").html(`You WON!  It took you ${moves} moves, idiot!`);
+  $("#gameOverModal").css("display", "block");
+}
+
+//close modal, start new game
+close.on("click", function() {
+  $("#gameOverModal").css("display", "none");
+  restart();
+});
 // when restart button clicked
-function restart(){
-    startGame();
-};
-
-
+function restart() {
+  matchedCard = [];
+  startGame();
+}
